44cd8db5bae1b5cfcc1a4635c1df0a32f63c30b2
rokatee <108390441+rokatee@users.noreply.github.com>
2022-06-29 16:52:57 +0900

    22.06.29
    Day1

---
+++ Test002.java
@@ -0,0 +1,27 @@
+// 자바 기본 프로그래밍
+
+public class Test002
+{
+	public static void main(String[] args)
+	{
+		System.out.println("안녕하세요.");
+		System.out.println("김인교 입니다.");
+		System.out.println("오늘은 6월 29일 입니다.");
+		System.out.println();
+		System.out.print("반갑습니다.");
+		System.out.print("화이팅 합시다.");
+		
+		/* 
+		println() 과 print() 메소드는
+		같은 상자에서 꺼낸 유사한 도구이지만
+		이 도구는 서로 다른 도구이며,
+		두 메소드의 차이는
+		라인 스킵(줄바꿈) 유무
+		*/
+	}
+}
+
+/*
+에러가 있을 경우 class 확장자가 생성되지 않는다.
+코드 수정 후 컴파일을 새로 하지 않으면 수정 내용이 적용되지 않는다.
+*/
