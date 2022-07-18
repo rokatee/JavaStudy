00d2805d7a913c4d6973673ceffedb66956ac201
Levi <rokatee@gmail.com>
2022-07-19 08:59:00 +0900

    fix

--- Test106_1.java
+++ Test106_1.java
@@ -70,7 +70,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class Test106
+public class Test106_1
 {
 	public static void main(String[] args) throws IOException
 	{
@@ -99,55 +99,39 @@ public class Test106
 		*/
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		// 입력받은 주민번호
+		
 		String str;
 
-		int[] times = {2, 3, 4, 5, 6, 7, 0, 8, 9, 2, 3, 4, 5};
+		int[] temp = {2, 3, 4, 5, 6, 7, 0, 8, 9, 2, 3, 4, 5};
 
-		// 곱셈 연산 후 누적합
-		int tot = 0;
+		int sum = 0;
 
 		System.out.print("주민번호 입력(xxxxxx-yyyyyy) : ");
 		str = br.readLine();
-		
+
 		// 주민번호 길이가 숫자(13개)와 "-"(1개)의 합인 14자리인지 확인
 		if (str.length() != 14)
 		{
 			System.out.println(">> 입력오류~!!!");
 			return;
 		}
-		
-		// 13자리 숫자 반복문 돌리며 times 배열과 주민번호 각자리 숫자 곱들의 누적합
+
 		for (int i = 0; i < 13; i++)
 		{
-			if (i == 6)		// 6번째 자리 '-'를 패스하기 위함
+			if (i == 6)		// '-' 부분을 패스하기 위한 구문
 				continue;
-
-			tot += times[i] * Integer.parseInt(str.substring(i, (i+1)));	// str.substring(m,n)은 
-																			// 실상 m부터 n-1까지 숫자를 의미하므로
-																			// i, i+1로 설정해야한다
-																			// 1,2(i+1) 이면 1부터 2의 전까지 → 1
-																			// 2,3		이면 2부터 3의 전까지 → 2
-																			// 각 자리 수마다 times 배열과 곱하기 가능~
+			sum += temp[i] * Integer.parseInt(str.substring(i,(i+1)));
 		}
 
-		//=================================================== 여기까지 수행하면 ① 과 ② 끝
-		
-		// ③. 더해진 결과값을 11로 나누어 '나머지'를 취한다.
-		// ④. 11에서 ③에서 나온 나머지를 뺀 결과값을 구한다
-		int result = 11 - tot % 11;
+		int total = 11 - sum % 11;
+		if(total == 10) total = 0;
+		if(total == 11) total = 1;
 
-		// ③의 처리 과정에서 나머지가 0인 경우 11로 바꾸고 1인 경우 10로 바꾼 뒤,
-		// 이를 다시 10으로 나누어 나머지를 취한다
-		result = result % 10;
-
-		//=================================================== 여기까지 수행하면 ③ 과 ④ 끝
-
-		// 최종 결과 출력
-		if (result == Integer.parseInt(str.substring(13)))		// str.substring(13)는 13번째 숫자부터 14번째 전까지
-			System.out.println(">> 정확한 주민번호~!!");		// result의 숫자가 str에 있는 13번째 숫자와 동일하면 굿
+		if (total == Integer.parseInt(str.substring(13)))
+			System.out.println("정확한 주민번호");
 		else
-			System.out.println(">> 잘못된 주민번호~!!");		// 아니면 ㅠㅠ...
+			System.out.println("잘못된 주민번호");
+
+		
 	}
 }
