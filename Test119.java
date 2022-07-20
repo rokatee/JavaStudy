6749a0dac3970f645d45f00e685d433d75c51df8
Levi <rokatee@gmail.com>
2022-07-21 08:58:12 +0900

    add

--- Test119.java
+++ Test119.java
@@ -1,7 +1,7 @@
-/*
-■■■ 클래스 고급 ■■■
-	- 인터페이스(Interface)
-*/
+/*===========================
+  ■■■ 클래스 고급 ■■■
+  - 인터페이스(Interface)
+===========================*/
 
 // 인터페이스
 interface Demo
@@ -10,17 +10,65 @@ interface Demo
 	public void print();
 }
 
-class DemoImpl implements Demo
+
+// 인터페이스를 구현하는 추상 클래스
+abstract class DemoImp1 implements Demo
 {
+	@Override
+	public void write()
+	{
+		System.out.println("write() 메소드 재정의...");
+	}
+
+	//public void print();
 }
 
-class DemoImplSub extends DemoImpl
+
+// 추상 클래스를 상속받는 추상 클래스
+//abstract class DemoImp1Sub extends DemoImp1
+class DemoImp1Sub extends DemoImp1
 {
+	/*
+	@Override
+	public void write()
+	{
+		System.out.println("write() 메소드 재정의...");
+	}
+	*/
+
+	@Override
+	public void print()
+	{
+		System.out.println("print() 메소드 재정의...");
+	}
 }
 
+
+// main() 메소드를 포함하는 외부의 다른 클래스
 public class Test119
 {
 	public static void main(String[] args)
 	{
+		//Demo ob1 = new Demo();
+		//-- 인스턴스 생성 불가 → 인터페이스
+
+		//DemoImp1 ob2 = new DemoImp1();
+		//-- 인스턴스 생성 불가 → 추상 클래스
+
+		DemoImp1Sub ob3 = new DemoImp1Sub();
+
+		ob3.write();
+		ob3.print();
+		//--==>> write() 메소드 재정의...
+		//--==>> print() 메소드 재정의...
+
 	}
 }
+
+// 실행 결과
+
+/*
+write() 메소드 재정의...
+print() 메소드 재정의...
+계속하려면 아무 키나 누르십시오 . . .
+*/
